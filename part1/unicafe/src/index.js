11cb9b023539be37189a7d07152ded59f9daa6f2
jacob <jacobinmt47@outlook.com>
2019-08-30 11:39:02 -0600

    got list of review

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,13 +7,24 @@ const [good,setGood] = useState(0)
 const [neutral,setNeutral] = useState(0)
 const [bad,setBad] = useState(0)
 
+// callback handlers
+const setG = () => {
+    return ( setGood(good +1))
+}
+const setN = () => {
+    return(setNeutral(neutral +1))
+}
+const setB = () =>{
+    return (setBad(bad +1))
+}
+
 
 return(
     <div>
         <h1>Give Feedback</h1><br />
-        <button onClick={() =>setGood(good +1)}>good</button>
-        <button onClick={() =>setNeutral(neutral +1)}>neutral</button>
-        <button onClick={() =>setBad(bad +1)}>bad</button><br />
+        <button onClick={setG}>good</button>
+        <button onClick={setN}>neutral</button>
+        <button onClick={setB}>bad</button><br />
         <h1>statistics</h1>
         <ul>
             <li>good  {good}</li>
