e81fe15ee328b8122e373259e8ed0302515a4be7
jacob l <jacobinmt47@outlook.com>
2019-08-29 20:14:23 -0600

    has problem with multiable counter

--- part1/e1/src/index.js
+++ part1/e1/src/index.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const Hello = ({name , age}) =>{
-    const bornYear = () => new Date().getFullYear() -age ;
-    return(
-        <div>
-            <p>
-                Hello {name} you are {age} years old <br />
-                you are probably  born in  {bornYear()}
-            </p>
-        </div>
-    )
+const App = (props) => {
+  const [ counter, setCounter ] = useState(0)
+
+  setTimeout(
+    () => setCounter(counter + 1),1000,
+    console.log(counter)
+  )
+
+  return (
+    <div>{counter}
+    <button onClick={() => setCounter(counter+1)}>
+        plus
+    </button>
+    <button onClick={() => setCounter(0)}>
+        zero
+    </button>
+    </div>
+  )
 }
 
-const App = (props) =>{
-    const name = "Peter";
-    const age = 10;
-    return (
-        <div>
-            <h1>Greetings</h1>
-            <Hello name="maya"  age={26+10}/>
-            <Hello name={name}  age={age}/>
-        </div>
-    )
-}
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <App />, 
+  document.getElementById('root')
+)
