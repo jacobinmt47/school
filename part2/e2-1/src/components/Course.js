59c952283b80f9a4a800b9f83c5c90669c68b9a4
jacob l <jacobinmt47@outlook.com>
2019-09-01 16:19:06 -0600

    works for one course

--- part2/e2-1/src/components/Course.js
+++ part2/e2-1/src/components/Course.js
@@ -1,19 +1,29 @@
 import React from 'react';
+const Sum = (props) =>{
+    return (
+        <li><h2>Total exercises:{props.sum}</h2></li>
+    )
+}
 
 const Parts = (props) =>{
     const rows = props.courses.map(p =><li key ={p.id}>{p.name}  {p.exercises}</li> )
     return(
         rows
     )
-}
+}   
 
 const Course =(props) =>{
     const course = props.course
-return(
+    const array = course.parts.map(e =>e.exercises)
+    console.log(array)
+    const sum =((s,p) =>  s + p)
+    const total = array.reduce(sum)
+    return(
     <div>
     <h1>{course.name}</h1>
     <ul>
     <Parts courses={course.parts}/>
+    <Sum sum={total}/>
     </ul>
     </div>
 )
