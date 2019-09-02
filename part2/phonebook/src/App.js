b24f5463ae5505cf933b62d40b637b6d76bff4ce
jacob l <jacobinmt47@outlook.com>
2019-09-02 15:23:15 -0600

    can add names

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,47 @@
 import React,{useState} from 'react';
 import './App.css';
+const Names= (props) =>{
+  const people = props.persons
+  const p = people.map(x =><li key={x.id}>{x.name}</li>)
+  return(p)
+}
 
 function App() {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      id:0
+    }
   ]) 
   const [ newName, setNewName ] = useState('')
+
+  const addName = (event) =>{
+    event.preventDefault()
+    const p ={
+      name:newName,
+      id:persons.length
+    }
+    console.log(p)
+    const x = persons.concat(p)
+    setPersons(x)
+    setNewName('')
+  } 
+  const handlePersonChange =(event) =>{
+    setNewName(event.target.value)
+  }
 
   return (
     <div className="App">
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit ={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handlePersonChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <Names persons={persons}  />
     </div>
   );
 }
