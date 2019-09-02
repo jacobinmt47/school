4cc16df059b90d8a8d9d3f2f7b8d8d6c4f5db770
jacob l <jacobinmt47@outlook.com>
2019-09-02 13:51:49 -0600

    changed index.js

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,10 +6,19 @@ function App() {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
-  
+
   return (
     <div className="App">
-  
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
     </div>
   );
 }
