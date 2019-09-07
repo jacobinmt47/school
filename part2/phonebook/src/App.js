6f3349b9550d3207260d5e41e47f81a6dbc9f646
jacob l <jacobinmt47@outlook.com>
2019-09-07 14:57:12 -0600

    comment out init data for persons

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -41,18 +41,19 @@ const PersonForm =(props) =>{
 const  App = () => {
   
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', phoneNumber: '040-123456',id:0 },
+   /* { name: 'Arto Hellas', phoneNumber: '040-123456',id:0 },
     { name: 'Ada Lovelace', phoneNumber: '39-44-5323523',id:1 },
     { name: 'Dan Abramov', phoneNumber: '12-43-234345',id:2 },
     { name: 'Mary Poppendieck', phoneNumber: '39-23-6423122',id:3 }
+    */
   ]) 
   useEffect(() =>{
     console.log("effect called")
     axios.get('http://localhost:3001/persons')
     .then(response =>{
       console.log("promise fullfilled")
-      console.log(response)
-      setPersons(response.persons)
+      console.log(response.data)
+      setPersons(response.data)
     })
   }
   )
