d4aeaf331deecd0fb992e28034912e24c14b70e0
jacob l <jacobinmt47@outlook.com>
2019-09-07 18:32:31 -0600

    can't get list of countries to load

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,21 +2,57 @@ import React,{useEffect,useState} from 'react';
 import axios from 'axios';
 import './App.css';
 
-App = () =>{
-  const[contries,setCountries] =useState(0)
+const Search =(props) =>{
+  return(
+    <>
+    <form>
+      <input value={props.filterName} onChange={props.handleFilterChange}/>
+    </form>
+    </>
+  )
+}
+
+const Countries =(props) =>{
+  const cntry = props.countries
+  const filter = props.filter
+  console.log(cntry)
+  if(typeof(cntry) ==='undefined'){
+    return(<>
+    space holding
+    </>)}
+  const c = cntry.Map(x =><li>{x.name}</li>)
+  return(
+    <>
+    c
+    </>
+  )
+}
+const App = () =>{
+  const[countries,setCountries] =useState('')
+  const[filter,setFilter] = useState('')
 
   useEffect(()=>{
     console.log("Effect called")
     axios.get("http://restcountries.eu/rest/v2/all").then(
       (response)=>{
-        console.log(response.data,"promise kept --")
+        console.log("promise kept --")
         setCountries(response.data)
       }
     )
   },[])
 
+  const handleFilterChange = (event) =>{
+    setFilter(event.target.value)
+  }
+  
+  
+
   return (
     <div>
+      <Search filterName ={filter} handleFilterChange={handleFilterChange} />
+      <ol>
+      <Countries cntry ={countries} filter={filter} />
+      </ol>
     </div>
   );
 }
