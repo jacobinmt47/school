ceef4bcf1bc310d075d12652a09dc1953566f737
jacob l <jacobinmt47@outlook.com>
2019-09-10 14:28:27 -0600

    update notes.js

--- part2/e2-2/src/App.js
+++ part2/e2-2/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import Note from './components/Note'
-import axios from 'axios'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([]) 
@@ -8,19 +8,19 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        setNotes(response.data)
-      })
+    noteService
+    .getAll()
+    .then(response => {
+      setNotes(response)
+    })
   }, [])
 const toggleImportanceOf = id =>{
-  const url = `http://localhost:3001/notes/${id}`
   const note = notes.find(n => n.id === id)
   const changedNote = { ...note, important: !note.important }
-
-  axios.put(url, changedNote).then(response => {
-    setNotes(notes.map(note => note.id !== id ? note : response.data))
+  noteService
+  .update(id, changedNote)
+  .then(response => {
+    setNotes(notes.map(note => note.id !== id ? note : response))
   })
 }
 
@@ -50,9 +50,10 @@ const toggleImportanceOf = id =>{
       id: notes.length + 1,
     }
 
-    axios.post('http://localhost:3001/notes',noteObject)
-    .then(response =>{console.log(response)
-      setNotes(notes.concat(noteObject))
+    noteService
+    .create(noteObject)
+    .then(initialNotes => {
+      setNotes(notes.concat(initialNotes))
       setNewNote('')
     })
   }
